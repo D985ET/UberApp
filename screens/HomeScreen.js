fb7ccdabb17377ed6e3b459460b7853ee80f7978
Sipos Levente <91262000+D985ET@users.noreply.github.com>
2022-08-01 09:07:19 +0200

    Building the NavOptions Component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,21 @@
 import { StyleSheet, Text, View, SafeAreaView,Image } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
 // <Text style={[tw`text-red-500 pt-11`]}>I am the homescreen!</Text>
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}> 
     <View style={tw`pt-5 pl-5`}>
-        <Image 
-        style={{width:100,height:100,resizeMode:'contain',}}
-        source= {{uri: "https://links.papareact.com/gzs",}}/>
+        <View style={tw`pl-3`}>
+          <Image 
+          style={{width:100,height:100,resizeMode:'contain'}}
+          source= {{uri: "https://links.papareact.com/gzs",}}/>
+        </View>
+        <NavOptions>
+
+        </NavOptions>
     </View>
     </SafeAreaView>
   );
